7ad6f9f35c668e3bb6efbc87c82c03b6094cd5fc
ryanpurdy <ryan.l.purdy@gmail.com>
2014-02-15 00:57:36 -0600

    save before trying something drastic
    added much css.

--- controllers/movies.js
+++ controllers/movies.js
@@ -2,7 +2,7 @@ var movies = angular.module('movies', []);
 
 movies.controller('movieController', function ($scope, $http) {
 
-    $http.jsonp('http://api.rottentomatoes.com/api/public/v1.0/movies/155655062.json', {
+    $http.jsonp('http://api.rottentomatoes.com/api/public/v1.0/movies/771303861.json', {
         params: {
             apikey: 'wq98h8vn4nfnuc3rt2293vru',
             callback: 'JSON_CALLBACK'
@@ -12,13 +12,6 @@ movies.controller('movieController', function ($scope, $http) {
         $scope.movies = data;
     });
      
-     
-     
-      //$scope.apiKey = "[wq98h8vn4nfnuc3rt2293vru]";
-      
-      //$http.jsonp('http://api.rottentomatoes.com/api/public/v1.0/movies/155655062.json?apikey=' + $scope.apiKey + '/?callback=JSON_CALLBACK')
-      //.success(function(data) 
-          // { $scope.movies = data;})
-       // .error(function(error) {});
  
-     });
+});
+
