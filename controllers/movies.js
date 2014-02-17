17e97ac2e665206fb80fda87eb7722d95931f4c3
ryan <ryan.l.purdy@gmail.com>
2014-02-16 20:39:11 -0600

    Transferring back to desktop
    Working on making buttons work.

---
+++ controllers/movies.js
@@ -0,0 +1,22 @@
+
+
+var movies = angular.module('movies', []);
+
+	movies.controller('movieController', function ($scope, $http) {
+	
+	    $http.jsonp('http://api.rottentomatoes.com/api/public/v1.0/movies/771303861.json', {
+	        params: {
+	            apikey: 'wq98h8vn4nfnuc3rt2293vru',
+	            callback: 'JSON_CALLBACK'
+	        }
+	    })
+	    .success(function (data) {
+	        $scope.movies = data;
+	    }); 
+	 
+	});
+    
+   movies.click('nowShowing', function ($scope){
+	   alert("asdasd");
+   });
+    
